462a4f7fbb21b553d980ced0d485c93c45244880
Dhiraj Borana <dhirajborana76@gmail.com>
2021-02-23 11:43:19 +0530

    Add dialog to create playlist

--- app/src/main/java/com/example/musicplayer/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/musicplayer/ui/library/LibraryFragment.kt
@@ -1,18 +1,20 @@
 package com.example.musicplayer.ui.library
 
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.musicplayer.R
 import com.example.musicplayer.data.model.Playlist
-import com.example.musicplayer.data.model.PlaylistSongCrossRef
 import com.example.musicplayer.databinding.FragmentLibraryBinding
 import com.example.musicplayer.ui.main.MainViewModel
-import com.example.musicplayer.ui.main.SongAdapter
+
 
 class LibraryFragment : Fragment() {
 
@@ -32,6 +34,28 @@ class LibraryFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         observeData()
         initPlaylistAdapter()
+        binding.fab.setOnClickListener {
+            Log.e("Library", "onCLick")
+            showCreatePlaylistDialog()
+        }
+    }
+
+    private fun showCreatePlaylistDialog() {
+        val input = EditText(requireContext()).apply { inputType = InputType.TYPE_CLASS_TEXT }
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("Playlist name")
+            setView(input)
+            setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            setPositiveButton("Ok") { _, _ -> createPlaylist(input.text.toString()) }
+            show()
+        }
+    }
+
+    private fun createPlaylist(playlistName: String) {
+        Log.e("Library", playlistName)
+        if (playlistName.isNotBlank() && playlistName.isNotEmpty()) {
+            viewModel.createPlaylist(Playlist(name = playlistName))
+        }
     }
 
     private fun observeData() {
@@ -43,7 +67,11 @@ class LibraryFragment : Fragment() {
     private fun initPlaylistAdapter() {
         playlistAdapter = PlaylistAdapter(object : PlaylistAdapter.PlaylistListener {
             override fun onPlaylistClicked(position: Int) {
-                val action = LibraryFragmentDirections.actionNavLibraryToNavPlaylist(viewModel.playlists.value?.get(position)?.playlist?.playlistId!!)
+                val action = LibraryFragmentDirections.actionNavLibraryToNavPlaylist(
+                    viewModel.playlists.value?.get(
+                        position
+                    )?.playlist?.playlistId!!
+                )
                 findNavController().navigate(action)
             }
         })
